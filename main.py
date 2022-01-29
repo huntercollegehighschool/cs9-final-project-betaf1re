02d801c706173b78d576a28fd8737bd9ddb6c05a
betaf1re <94643793+betaf1re@users.noreply.github.com>
2022-01-28 20:23:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from master import TicTacGrow
 from turtle import Screen
 
 
+#note: pls don't run this through replit because it makes it super weird and buggy (at least from my experience) + replit doesn't support most of the fonts used here so the tutorial will be messed up
+
+
 screen = Screen()
 screen.title("Tic Tac Grow")
 screen.setup(1200, 900)
